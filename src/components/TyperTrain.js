9106ba0c096b051100359a2194c2b934be43944f
Kushan Amugoda <116130909+Exoticbluebear@users.noreply.github.com>
2024-10-14 08:54:28 -0700

    Update TyperTrain.js

--- src/components/TyperTrain.js
+++ src/components/TyperTrain.js
@@ -29,7 +29,7 @@ const TyperTrain = () => {
     const [timeLeft, setTimeLeft] = useState(maxTime);
     const [charIndex, setCharIndex] = useState(0);
     const [mistakes, setMistakes] = useState(0);
-    const [isTyping, setIsTyping] = useState(false);
+    const [isTyping, setIsTyping] = useState(true);
     const [WPM, setWPM] = useState(0);
     const [CPM, setCPM] = useState(0);
     const inputRef = useRef();
