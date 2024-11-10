0e3eb26dc4d0cd647e280b1e9b0a4aa8155fa230
KA 💻 <116130909+Exoticbluebear@users.noreply.github.com>
2024-11-10 01:30:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import TyperTrain from './components/TyperTrain';
+import Darkmode from './components/Darkmode';
 
 function App() {
   return (
     <div className="App">
+      <Darkmode />
       <TyperTrain />
     </div>
   );
