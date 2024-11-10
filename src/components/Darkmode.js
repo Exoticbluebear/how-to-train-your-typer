c9281a76e33cff0bfc29ad587e135e1695641784
KA 💻 <116130909+Exoticbluebear@users.noreply.github.com>
2024-11-10 01:28:28 -0800

    Add files via upload
    Adding dark mode function

---
+++ src/components/Darkmode.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Darkmode.css";
+
+const Darkmode = () => {
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'dark')
+    }
+
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'light')
+    }
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) setDarkMode();
+        else setLightMode();
+    };
+
+    return (
+
+        <div className="darkmode-toggle">
+            <label class="switch">
+            <input 
+            type='checkbox'
+            onChange={toggleTheme}/>   
+            <span className="slider round"></span>     
+            </label>  
+            <div className="heading">
+                <h3>How-To-Train-Your-Typer</h3>
+            </div>
+            <div className="title">
+                <h4>An app by Kushan Amugoda</h4>
+            </div>    
+        </div>
+    )
+};
+
+export default Darkmode;
